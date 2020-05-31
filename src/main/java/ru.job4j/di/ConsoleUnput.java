2ff4d908666a67b74576124b556f4a7cedc304d6
EdgarGilmanov <www.edgar.fisher@gmail.com>
2020-05-31 09:07:20 +0500

    Время жизни Spring bean. Scope. [#291951]

--- src/main/java/ru.job4j/di/ConsoleUnput.java
+++ src/main/java/ru.job4j/di/ConsoleUnput.java
@@ -1,8 +1,9 @@
 package ru.job4j.di;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+@Scope("prototype")
 public class ConsoleUnput {
     public void print(String value) {
         System.out.println(value);
